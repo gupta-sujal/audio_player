fa73f9df3e9c1a9eb92ec01f25b391b666d6bab4
gupta-sujal <sujalgupta484@gmail.com>
2023-11-15 13:48:01 +0530

    changes done

--- static/scripts.js
+++ static/scripts.js
@@ -77,8 +77,6 @@
 //     audio.currentTime = seekTime;
 // });
 
-// Select all the elements in the HTML page
-// and assign them to a variable
 let now_playing = document.querySelector(".now-playing");
 // const audio=document.getElementById('audio')
 let playpause_btn = document.querySelector(".playpause-track");
@@ -89,8 +87,6 @@ let seek_slider = document.querySelector(".seek_slider");
 let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
-
-// Specify globally used values
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
@@ -113,7 +109,7 @@ let updateTimer;
           'image3.jpg',
           // Add more image URLs as needed
       ];
-function random_bg_color() {
+function image_change() {
     var element = document.getElementById('top-portion');
     fetch('/get_image_path')
         .then(response => response.text())
@@ -127,54 +123,48 @@ function random_bg_color() {
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
   }
-  random_bg_color()
+  image_change()
   function seekUpdate() {
     let seekPosition = 0;
-    
-    // Check if the current track duration is a legible number
-   
         seekPosition = curr_track.currentTime * (100 / curr_track.duration);
         seek_slider.value = seekPosition;
         if(seek_slider.value==100)
         nextTrack()
-        // Calculate the time left and the total duration
         let currentMinutes = Math.floor(curr_track.currentTime / 60);
         let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
         let durationMinutes = Math.floor(curr_track.duration / 60);
         let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
     
-        // Add a zero to the single digit time values
         if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
         if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
         if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
         if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
     
-        // Display the updated duration
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
         total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
   function playpauseTrack() {
-    // Switch between playing and pausing
-    // depending on the current state
     if (!isPlaying) playTrack();
     else pauseTrack();
     }
-    
+    function shuffle()
+    {
+      var ul = document.querySelector('#music-list ul');
+        for (var i = ul.children.length; i >= 0; i--) {
+            ul.appendChild(ul.children[Math.random() * i | 0]);
+        }
+    }
     function playTrack() {
-    // Play the loaded track
     curr_track.play();
     isPlaying = true;
     
-    // Replace icon with the pause icon
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
     }
     
     function pauseTrack() {
-    // Pause the loaded track
     curr_track.pause();
     isPlaying = false;
     
-    // Replace icon with the play icon
     playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
     }
     
@@ -194,7 +184,7 @@ function random_bg_color() {
             document.getElementById("display-track-name").textContent =name ; 
             curr_track=audio
             playTrack()
-            random_bg_color()
+            image_change()
             setVolume()
         });
     }
@@ -214,7 +204,7 @@ function random_bg_color() {
             document.getElementById("display-track-name").textContent =name ; 
             curr_track=audio
             playTrack()
-            random_bg_color()
+            image_change()
             setVolume()
         });
     }
@@ -245,7 +235,7 @@ function random_bg_color() {
             document.getElementById("display-track-name").textContent =name ; 
             curr_track=audio
             playTrack()
-            random_bg_color()
+            image_change()
             setVolume()
             
         });
@@ -264,7 +254,7 @@ function random_bg_color() {
         //         document.getElementById("display-track-name").textContent =name ; 
         //         curr_track=audio
         //         playTrack()
-        //         random_bg_color()
+        //         image_change()
         //     }, 
         //     error: function(error) { 
         //         console.log(error);
@@ -283,33 +273,20 @@ function random_bg_color() {
         //     document.getElementById("display-track-name").textContent =name ; 
         //     curr_track=audio
         //     playTrack()
-        //     random_bg_color()
+        //     image_change()
             
         // });
     }
     
     function seekTo() {
-        // Calculate the seek position by the
-        // percentage of the seek slider 
-        // and get the relative duration to the track
         seek = curr_track.duration * (seek_slider.value / 100);
-       
-        // Set the current track position to the calculated seek position
         curr_track.currentTime = seek;
         }
         
         function setVolume() {
-        // Set the volume according to the
-        // percentage of the volume slider set
         curr_track.volume = volume_slider.value / 100;
         }
-        function shuffle()
-        {
-          var ul = document.querySelector('#music-list ul');
-            for (var i = ul.children.length; i >= 0; i--) {
-                ul.appendChild(ul.children[Math.random() * i | 0]);
-            }
-        }
+       
         
         
             
